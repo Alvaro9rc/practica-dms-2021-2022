b935e3caab377013d22dc3dd5107aea807579064
AlbertoPorres <57088842+AlbertoPorres@users.noreply.github.com>
2021-11-27 12:38:23 +0100

    [add] añadidas rutas de volver

--- components/dms2122frontend/dms2122frontend/presentation/web/studentendpoints.py
+++ components/dms2122frontend/dms2122frontend/presentation/web/studentendpoints.py
@@ -2,7 +2,7 @@
 """
 
 from typing import Text, Union
-from flask import redirect, url_for, session, render_template
+from flask import redirect, url_for, session, render_template, request
 from werkzeug.wrappers import Response
 from dms2122common.data import Role
 from dms2122frontend.data.rest import BackendService
